8b391993a4ac650d7c1e58c16f18648175d0e31a
alehuelva <alehuelva13@gmail.com>
2012-05-08 17:53:03 +0200

    pruebas

---
+++ web/cosas sueltas.php
@@ -0,0 +1,32 @@
+    
+ // include 'ejemploxml.php';
+
+ // $peliculas = new SimpleXMLElement($xmlstr);
+  
+//  foreach ($peliculas->xpath('//personaje') as $personaje) {
+  //	echo $personaje->nombre . ' interpretado por ' . $personaje->actor, PHP_EOL;
+ 
+  ?>
+    
+  	
+  		
+  
+
+  //<root>
+  //<ns:book xmlns:ns=\"http://quickstart.samples/xsd\">
+  //<ns:title>Titulo</ns:title>
+  //</ns:book>
+  //<title>titlea</title>
+ // </root>";
+  
+  //$xml = simplexml_load_string( $xml );
+  //$ns = $xml->getNamespaces( true );
+  //$xml->registerXPathNamespace('c', $ns['ns']);
+  //$title = $xml->xpath('//c:title');
+  //echo $title[0];
+  ?>
+  
+  
+  <peliculas>
+   <pelicula>
+   <puntuacion tipo="pulgares">7</puntuacion>
---
+++ web/ejemploxml.php
@@ -0,0 +1,52 @@
+<?php
+$xmlstr = <<<XML
+<?xml version='1.0' standalone='yes'?>
+<peliculas>
+ <pelicula>
+  <titulo>PHP: Tras el Parser</titulo>
+  <personajes>
+   <personaje>
+    <nombre>Srta. Programadora</nombre>
+    <actor>Onlivia Actora</actor>
+   </personaje>
+   <personaje>
+    <nombre>Sr. Programador</nombre>
+    <actor>El Actor</actor>
+   </personaje>
+  </personajes>
+  <argumento>
+   Así que, este lenguaje. Es como, un lenguaje de programación. ¿O es un
+   lenguaje interpretado? Lo descubrirás en esta intrigante y temible parodia
+   de un documental.
+  </argumento>
+  <grandes-lineas>
+   <linea>PHP soluciona todos los problemas web</linea>
+  </grandes-lineas>
+  <puntuacion tipo="pulgares">7</puntuacion>
+  <puntuacion tipo="estrellas">5</puntuacion>
+ </pelicula>
+ <pelicula>
+  <titulo>PHP 2: Tras el Parser</titulo>
+  <personajes>
+   <personaje>
+    <nombre>Srta. Prepi</nombre>
+    <actor>Oliviatora</actor>
+   </personaje>
+   <personaje>
+    <nombre>Sr. Prador</nombre>
+    <actor>Etor</actor>
+   </personaje>
+  </personajes>
+  <argumento>
+   Ay temible parodia
+   de un documental.
+  </argumento>
+  <grandes-lineas>
+   <linea>PHP2 saca bum bum</linea>
+  </grandes-lineas>
+  <puntuacion tipo="pulgares">3</puntuacion>
+  <puntuacion tipo="estrellas">2</puntuacion>
+ </pelicula>
+</peliculas>
+XML;
+?>
